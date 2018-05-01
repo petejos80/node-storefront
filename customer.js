21191f1fb04c8d85382b452059935eabb2511c2d
petejos80 <petejos80@gmail.com>
2018-04-30 20:18:34 -0500

    Configured display of products with console.table

--- customer.js
+++ customer.js
@@ -1,11 +1,12 @@
-// DATABASE CONNECTION INFO
-// GLOBAL VARIABLES
-// ====================
+// ==================================================
+// DATABASE CONNECTION INFO + GLOBAL VARIABLES
+// ==================================================
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table")
 
-var connection = mysql.createConnection({
+var db = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
@@ -14,23 +15,60 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "",
-  database: "greatbay_db"
+  database: "storefront_db"
 });
 
-//Connection to database
+//Validate connection to database
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connected to Database!");
   });
 
 
-
+// ==================================================
 // FUNCTIONS
-// ====================
+// ==================================================
+
+//Exectues queries on DB
+function executeQuery(sql, cb) {
+    db.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        cb(result);
+    });
+}
+
+//Prints products table
+function fetchProducts(res){
+    db.query("SELECT * FROM products", function(err, result){
+        if (err) {
+            console.log(err)
+        } else {
+            console.table(result)
+        }
+
+    // res.write("<table>");
+    // res.write("<tr>");
+    // for(var column in result[0]){
+        // res.write("<td><label>" + column + "</label></td>");
+    // }
+    // res.write("</tr>");
+    // for(var row in result){
+        // res.write("<tr>");
+        // for(var column in result[row]){
+            // res.write("<td><label>" + result[row][column] + "</label></td>");       
+        // res.write("</tr>");         
+    })
+    // res.write("</table>");
+};
 
 
 
 
+// ==================================================
+// WORKFLOW
+// ==================================================
+
+fetchProducts();
 
 
 
